c652c507c63ff6dfa8aac07a20e4e8d8565261bc
amlgamal <amlgamal103@gmail.com>
2024-02-20 18:10:22 +0200

    tipDone

--- src/modules/tip/tipRoutes.js
+++ src/modules/tip/tipRoutes.js
@@ -1,11 +1,13 @@
 import express from 'express';
-import { createTip, getAllTips } from './tipController.js';
+import { createTip, getAllTips, deleteTip, updateTip } from './tipController.js';
 import {validate} from '../../middleware/tipValidate.js';
 import tipValidator from './tipValidator.js';
 
 const router = express.Router();
 
-router.post('/tip', validate(tipValidator), createTip);
-router.get('/tips', getAllTips);
+router.post('/addNewTip', validate(tipValidator), createTip);
+router.get('/getAllTips', getAllTips);
+router.delete('/deleteTip:id', deleteTip);
+router.put('/updateTip:id', updateTip);
 
 export default router;
