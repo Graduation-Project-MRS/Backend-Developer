daab73ec49456456ccbd233debbe8e2e7066ed54
mariam khaled <khaldmrym769@gmail.com>
2024-05-04 22:06:32 +0300

    update login response

--- src/modules/auth/controller/auth.js
+++ src/modules/auth/controller/auth.js
@@ -76,7 +76,7 @@ export const login = asyncHandler(async (req, res, next) => {
 
   const token = jwt.sign(
     { id: user._id, email: user.email },
-    process.env.TOKEN_SIGNATURE,
+    process.env.TOKEN_SIGNATURE
   );
 
   await tokenModel.create({
@@ -88,7 +88,13 @@ export const login = asyncHandler(async (req, res, next) => {
   user.status = "online";
   await user.save();
 
-  return res.status(200).json({ success: true, result: token });
+  return res
+    .status(200)
+    .json({
+      success: true,
+      token,
+      data: { userName: user.userName, profileImage: user.profileImage },
+    });
 });
 
 //send forget Code
@@ -109,7 +115,7 @@ export const sendForgetCode = asyncHandler(async (req, res, next) => {
   await user.save();
   const token = jwt.sign(
     { id: user._id, email: user.email },
-    process.env.TOKEN_SIGNATURE,
+    process.env.TOKEN_SIGNATURE
   );
   await tokenModel.create({
     token,
@@ -121,7 +127,9 @@ export const sendForgetCode = asyncHandler(async (req, res, next) => {
     subject: "Reset Password",
     html: resetPassword(code),
   }))
-    ? res.status(200).json({ success: true, message: "check you email!",token })
+    ? res
+        .status(200)
+        .json({ success: true, message: "check you email!", token })
     : next(new Error("Something went wrong!", { cause: 400 }));
 });
 
@@ -148,7 +156,7 @@ export const resetPasswordByCode = asyncHandler(async (req, res, next) => {
 
 export const VerifyCode = asyncHandler(async (req, res, next) => {
   const user = await userModel.findOne({ email: req.user.email });
-  if(!user.forgetCode){
+  if (!user.forgetCode) {
     return next(new Error("go to resend forget code", { status: 400 }));
   }
   if (user.forgetCode !== req.body.forgetCode) {
@@ -158,7 +166,7 @@ export const VerifyCode = asyncHandler(async (req, res, next) => {
     { email: req.user.email },
     { $unset: { forgetCode: 1 } }
   );
-  
+
   return res
     .status(200)
     .json({ success: true, message: "go to reset new password" });
